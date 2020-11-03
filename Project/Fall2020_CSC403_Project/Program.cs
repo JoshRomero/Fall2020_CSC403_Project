957178e3b06f0bf46fe9eb03400fb03a1372d16c
Josh Romero <JoshRomero@gmail.com>
2020-11-02 18:31:24 -0600

    Update Program.cs
    test

--- Project/Fall2020_CSC403_Project/Program.cs
+++ Project/Fall2020_CSC403_Project/Program.cs
@@ -6,10 +6,11 @@ using System.Windows.Forms;
 
 namespace Fall2020_CSC403_Project {
   static class Program {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
+        /// <summary>
+        /// The main entry point for the application.
+        /// /// this is a test
+        /// </summary>
+        [STAThread]
     static void Main() {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
