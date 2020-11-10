dfda5345988ae68651ab2af5bd4e824d977f5098
Josh Romero <JoshRomero@gmail.com>
2020-11-10 16:33:17 -0600

    added a form for Linh

--- Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
+++ Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace Fall2020_CSC403_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap died {
+            get {
+                object obj = ResourceManager.GetObject("died", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Dracula {
             get {
                 object obj = ResourceManager.GetObject("Dracula", resourceCulture);
