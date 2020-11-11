5c850d6a0c206e3cab0034347fde7ecaa79639ea
Linh Nguyen <tuonglinhchelsea@gmail.com>
2020-11-11 13:56:00 -0600

    working on making the end screen pops up

--- Project/Fall2020_CSC403_Project/death.cs
+++ Project/Fall2020_CSC403_Project/death.cs
@@ -7,17 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Fall2020_CSC403_Project.code;
 
 namespace Fall2020_CSC403_Project
 {
     public partial class death : Form
     {
         public static death instance = null;
+        private Enemy enemy;
         public death()
         {
             InitializeComponent();
         }
-        private void death_Load(object sender, EventArgs e)
+
+        public void death_Load(object sender, EventArgs e)
         {
 
         }
